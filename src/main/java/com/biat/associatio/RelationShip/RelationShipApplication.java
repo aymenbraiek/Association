0aae23f5363729c9fd0f08201495a19219a1379b
aymenbraiek <aymen.braiek@esprit.tn>
2021-07-02 19:50:35 +0100

    Intial Adding Project closes #2

---
+++ src/main/java/com/biat/associatio/RelationShip/RelationShipApplication.java
@@ -0,0 +1,13 @@
+package com.biat.associatio.RelationShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationShipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biat/associatio/RelationShip/RelationShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biat.associatio.RelationShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
